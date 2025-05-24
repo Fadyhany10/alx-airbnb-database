432a8a824924d4e371f58a116c271cd750f1680c
fady hany <104906845+Fadyhany10@users.noreply.github.com>
2025-05-24 20:17:42 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -40,3 +40,30 @@ JOIN
     properties p ON b.property_id = p.id
 LEFT JOIN 
     payments pay ON b.id = pay.booking_id;
+
+-- Initial complex query with WHERE and AND
+EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    b.booking_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email AS user_email,
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+JOIN 
+    payments pay ON b.id = pay.booking_id
+WHERE 
+    pay.status = 'completed'
+    AND b.booking_date >= '2024-01-01';
+
