b4c6746e06eff4807539b68f8c2d9626daa7e1e6
fady hany <104906845+Fadyhany10@users.noreply.github.com>
2025-05-24 20:13:20 +0300

    Create  perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,38 @@
+-- Initial Complex Query
+SELECT 
+    b.id AS booking_id,
+    b.booking_date,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email AS user_email,
+    p.id AS property_id,
+    p.name AS property_name,
+    p.location,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+JOIN 
+    payments pay ON b.id = pay.booking_id;
+
+-- Refactored Query
+SELECT 
+    b.id AS booking_id,
+    b.booking_date,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount,
+    pay.status
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+LEFT JOIN 
+    payments pay ON b.id = pay.booking_id;
